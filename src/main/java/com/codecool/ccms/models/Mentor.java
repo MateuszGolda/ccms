89dccac7b985f9f157db098de8196d4459b9b497
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-05-18 21:44:57 +0200

    implement setController method

--- src/main/java/com/codecool/ccms/models/Mentor.java
+++ src/main/java/com/codecool/ccms/models/Mentor.java
@@ -1,5 +1,7 @@
 package com.codecool.ccms.models;
 
+import com.codecool.ccms.controllers.MentorMenuController;
+
 public class Mentor extends User {
     public Mentor(int id, String name, String surname, String email) {
         super(id, name, surname, email);
@@ -8,5 +10,9 @@ public class Mentor extends User {
     @Override
     protected void displayMainMenu() {
 
+    }
+    @Override
+    public void setController() {
+        setMenuController(new MentorMenuController());
     }
 }
