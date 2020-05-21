3882367910cd66033e7c0dfc98e022a699b23947
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-05-21 13:11:41 +0200

    all methods for mentor implemented

--- src/main/java/com/codecool/ccms/controllers/ManagerMenuController.java
+++ src/main/java/com/codecool/ccms/controllers/ManagerMenuController.java
@@ -1,5 +1,8 @@
 package com.codecool.ccms.controllers;
 
+import com.codecool.ccms.dao.UserDao;
+import com.codecool.ccms.session.Registration;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,23 +44,56 @@ public class ManagerMenuController implements  MenuController{
     }
 
     private void addMentor() {
+        String[] data = gatherMentorData();
+        UserDao.getInstance().insert(data);
+        ui.gatherEmptyInput("Mentor successfully added!");
+    }
 
+    private String[] gatherMentorData() {
+        String name = ui.gatherInput("Enter mentor name: ");
+        String surname = ui.gatherInput("Enter mentor surname: ");
+        String password = ui.gatherInput("Enter mentor password: ");
+        String email = ui.gatherInput("Enter mentor email: ");
+        String mentor_id = "2"; //default id for mentor
+        return new String[] { name, surname, password, email, mentor_id };
     }
 
     private void removeMentor() {
+        displayMentorsList();
+        String id = ui.gatherInput("Enter id to remove mentor: ");
+        UserDao.getInstance().remove("users", id);
 
     }
 
     private void editMentor() {
+        displayMentorsList();
+        String id = ui.gatherInput("Enter id to edit mentor: ");
+        UserDao.getInstance().print("*","id = " + id);
+        Map<Integer, String> mentorFields = mentorFieldsMap();
+        ui.printMap(mentorFields);
+        int userChoice = ui.gatherIntInput("What field would you like to change",1, mentorFields.size());
+        String newValue = ui.gatherInput("Enter new value for '" + mentorFields.get(userChoice)) + "': ";
+        UserDao.getInstance().update(mentorFields.get(userChoice), newValue,"id = " + id);
 
+    }
+    public Map<Integer, String> mentorFieldsMap() {
+        Map<Integer, String> mentorFields = new HashMap<>();
+        mentorFields.put(1, "name");
+        mentorFields.put(2, "surname");
+        mentorFields.put(3, "password");
+        mentorFields.put(4, "email");
+        mentorFields.put(5, "id_role");
+        return mentorFields;
     }
 
     private void displayMentorsList() {
+        UserDao.getInstance().print("*","id_role = 2");
 
     }
 
     private void displayStudentsList() {
-
+        //ui.printTableFromDB(UserDao.getInstance().resultSetFromQuery("SELECT ")); //todo print table of students to se how they perform
+        UserDao.getInstance().print("*","id_role = 1");
     }
 
     private void logout() {
