92f39bf78114b64120a2641bd636acdabb10e70a
MateuszGolda <mateusz.golda1993@gmail.com>
2020-05-21 10:50:56 +0200

    create 1 connection to db, and close it when program closes

--- src/main/java/com/codecool/ccms/Main.java
+++ src/main/java/com/codecool/ccms/Main.java
@@ -1,17 +1,24 @@
 package com.codecool.ccms;
 
 import com.codecool.ccms.dao.ConnectionToDB;
-import com.codecool.ccms.dao.IConnection;
+import com.codecool.ccms.dao.ConnectionHandler;
 import com.codecool.ccms.session.Session;
+
+import java.sql.SQLException;
 
 /**
  * Main
  */
 public class Main {
-    public static IConnection iConnection;
-    public static void main( String[] args )
-    {
-        iConnection = new ConnectionToDB("src/main/resources/ccmsDB");
-        new Session();
+    public static ConnectionHandler connectionHandler;
+    public static void main( String[] args ) throws SQLException {
+        try {
+            connectionHandler = new ConnectionToDB("src/main/resources/ccmsDB");
+            connectionHandler.connect();
+            new Session();
+        } finally {
+            connectionHandler.getStatement().close();
+            connectionHandler.getConnection().close();
+        }
     }
 }
