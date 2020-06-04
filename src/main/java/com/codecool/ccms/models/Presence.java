7f482b40a677c76f60dd9f87ffc2022ac0673670
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-06-04 16:30:38 +0200

    constructor and getter add

--- src/main/java/com/codecool/ccms/models/Presence.java
+++ src/main/java/com/codecool/ccms/models/Presence.java
@@ -1,5 +1,15 @@
 package com.codecool.ccms.models;
 
 public enum Presence {
-    PRESENT, ABSENT, LATE, EXCUSED
+    PRESENT(1), ABSENT(2), LATE(3), EXCUSED(4);
+
+    private final int value;
+
+    Presence(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
